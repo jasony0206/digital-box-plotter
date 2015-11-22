7e790b9e65059c5e0efd8f12ff9542130a908e2f
jasony0206 <jasony0206@gmail.com>
2015-11-21 23:25:46 -0800

    Add force and pressure graph buttons with layout

--- src/main/java/com/aiaa/urp/electronics/MasterWindow.java
+++ src/main/java/com/aiaa/urp/electronics/MasterWindow.java
@@ -1,14 +1,16 @@
 package com.aiaa.urp.electronics;
 
-import java.awt.EventQueue;
-import javax.swing.JFrame;
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import javax.swing.*;
 
 public class MasterWindow extends JFrame {
 
     // This class represents the master window of the application.
     // It has buttons that open a new window for graphing when pressed.
-    // A newly opened window will display a time-value line graph
-    // live from the data returned by the master device.
+    // A newly opened window will plot a time-value line graph
+    // real time from the data returned by the master device.
 
     public MasterWindow() {
         initUI();
@@ -17,10 +19,51 @@ public class MasterWindow extends JFrame {
     private void initUI() {
         setTitle("AIAA URP Digital Box Plotter");
         setSize(400, 300);
-        // center the window on the screen
         setLocationRelativeTo(null);
-        // exit when close button is pressed
         setDefaultCloseOperation(EXIT_ON_CLOSE);
+
+        // Create buttons for plotting graphs
+        JButton forceGraphButton = new JButton("Force");
+        JButton pressureGraphButton = new JButton("Pressure");
+
+        forceGraphButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent event) {
+                System.out.println("force button pressed!");
+            }
+        });
+
+        pressureGraphButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent event) {
+                System.out.println("pressure button pressed!");
+            }
+        });
+
+        createLayout(forceGraphButton, pressureGraphButton);
+    }
+
+    private void createLayout(JComponent... arg) {
+        // Content pane is an area where child components can be placed
+        Container pane = getContentPane();
+
+        // Child components are organized by a layout manager.
+        // Here, we use a type called GroupLayout manager.
+        GroupLayout layout = new GroupLayout(pane);
+        pane.setLayout(layout);
+
+        // Leave margin between components
+        layout.setAutoCreateContainerGaps(true);
+
+        layout.setHorizontalGroup(layout.createParallelGroup()
+                        .addComponent(arg[0])
+                        .addComponent(arg[1])
+        );
+
+        layout.setVerticalGroup(layout.createSequentialGroup()
+                        .addComponent(arg[0])
+                        .addComponent(arg[1])
+        );
     }
 
     public static void main(String[] args) {
